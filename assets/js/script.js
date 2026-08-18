909793baba1f1988d9121d1fbb924231ccebec9c
DevSazu <shazahansazu@gmail.com>
2026-08-19 00:24:40 +0600

    category part end

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,14 @@ const categoriesTree=()=>{
 
 const showCategories=(trees)=>{
  
-    const ul=document.getElementById('categories')
+    const cate=document.getElementById('categories')
     
      trees.forEach(tree => {
-       const li= document.createElement('li')
-        li.className='w-11/12 p-2 max-auto block hover:text-white hover:bg-[#15803D]'
-       li.innerHTML=`${tree.category_name}`
-        ul.appendChild(li)
-        console.log(ul)
+       const ul= document.createElement('ul')
+        
+       ul.innerHTML=`<li onclick="loadCtegori(${tree.id})" class="w-11/12 p-2 mx-auto block hover:text-white hover:bg-[#15803D] category-btn" id="btn-${tree.id}"> ${tree.category_name} </li>`
+        cate.appendChild(ul)
+       
      });
 }
 
@@ -24,7 +24,7 @@ categoriesTree()
 
 
 const treeDetails=()=>{
-   const detailsUrl="https://openapi.programming-hero.com/api/category/1"
+   const detailsUrl="https://openapi.programming-hero.com/api/plants"
    fetch(detailsUrl)
    .then(res=>res.json())
    .then(data=>{
@@ -34,14 +34,15 @@ const treeDetails=()=>{
 
 const displayDetails=(cards)=>{
   const showTree=document.getElementById('tree_details')
+   showTree.innerHTML="";
   
   cards.forEach(card =>{
      const div=document.createElement('div');
-         div.className="card bg-base-100 w-full shadow-sm";
+    
          div.innerHTML=`
                       <div class="card bg-base-100 w-full shadow-sm">
                         <figure>
-                            <img src="${card.image} " class="h-[200px] w-full"
+                            <img src="${card.image} " class="h-[200px] w-full object-cover"
                                 alt="Shoes" />
                         </figure>
                         <div class="card-body">
@@ -49,7 +50,7 @@ const displayDetails=(cards)=>{
                                 ${card.name}
                                 
                             </h2>
-                            <p class="h-[50px] pt-2 pb-2">${card.description}</p>
+                            <p class="h-[70px] pt-2 pb-2">${card.description}</p>
                              <div class="badge badge-secondary">${card.category}</div>
                             
                                 <button class="btn bg-[#15803D] w-full text-white">Add to Cart</button>
@@ -63,3 +64,56 @@ const displayDetails=(cards)=>{
 
 }
 treeDetails();
+
+const loadCtegori=(id)=>{
+
+   const allButtons=document.querySelectorAll(".category-btn");
+
+   allButtons.forEach(btn=>{
+      btn.classList.remove('bg-[#15803D]','text-white')
+   });
+
+   const activeBtn = document.getElementById(`btn-${id}`);
+    if (activeBtn) {
+        activeBtn.classList.add('bg-[#15803D]', 'text-white');
+    }
+
+   const catTreeurl=`https://openapi.programming-hero.com/api/category/${id}`
+   fetch (catTreeurl)
+   .then(res=>res.json())
+   .then(data=>{
+      showTreeByCategory(data.plants)
+      
+   })
+}
+
+const showTreeByCategory=(catTrees)=>{
+   const showTree=document.getElementById('tree_details');
+    showTree.innerHTML="";
+  catTrees.forEach(catTree=>{
+  
+    const div=document.createElement('div');
+    
+         div.innerHTML=`
+                      <div class="card bg-base-100 w-full shadow-sm">
+                        <figure>
+                            <img src="${catTree.image} " class="h-[200px] w-full object-cover"
+                                alt="Shoes" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">
+                                ${catTree.name}
+                                
+                            </h2>
+                            <p class="h-[70px] pt-2 pb-2">${catTree.description}</p>
+                             <div class="badge badge-secondary">${catTree.category}</div>
+                            
+                                <button class="btn bg-[#15803D] w-full text-white">Add to Cart</button>
+                            
+                        </div>
+                    </div>
+  `
+   showTree.appendChild(div)
+   
+  });
+}
