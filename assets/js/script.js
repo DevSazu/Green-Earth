9a3e6512f494316ff9245167c9d097ee3fc57d1b
DevSazu <sazushazahan@gmail.com>
2026-08-18 21:28:08 +0600

    git update

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,45 @@ const showCategories=(trees)=>{
 }
 
 categoriesTree()
+
+
+const treeDetails=()=>{
+   const detailsUrl="https://openapi.programming-hero.com/api/category/1"
+   fetch(detailsUrl)
+   .then(res=>res.json())
+   .then(data=>{
+      displayDetails(data.plants)
+   })
+}
+
+const displayDetails=(cards)=>{
+  const showTree=document.getElementById('tree_details')
+  
+  cards.forEach(card =>{
+     const div=document.createElement('div');
+         div.className="card bg-base-100 w-full shadow-sm";
+         div.innerHTML=`
+                      <div class="card bg-base-100 w-full shadow-sm">
+                        <figure>
+                            <img src="${card.image} " class="h-[200px] w-full"
+                                alt="Shoes" />
+                        </figure>
+                        <div class="card-body">
+                            <h2 class="card-title">
+                                ${card.name}
+                                
+                            </h2>
+                            <p class="h-[50px] pt-2 pb-2">${card.description}</p>
+                             <div class="badge badge-secondary">${card.category}</div>
+                            
+                                <button class="btn bg-[#15803D] w-full text-white">Add to Cart</button>
+                            
+                        </div>
+                    </div>
+  `
+   showTree.appendChild(div)
+  });
+  
+
+}
+treeDetails();
